e9834c3e1ffeeb3a4f4dbb8b5986c7c254b3fb57
陳雅雲 <amily52013140618@gmail.com>
2021-06-03 21:16:39 +0800

    model

--- Web410716500/ViewModels/SignUpData.cs
+++ Web410716500/ViewModels/SignUpData.cs
@@ -7,5 +7,12 @@ namespace Web410716500.ViewModels
 {
     public class SignUpData
     {
+        public string Name { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string NameMessage { get; set; }
+        public string AccountMessage { get; set; }
+        public string PasswordMessage { get; set; }
+        public string Message { get; set; }
     }
 }
